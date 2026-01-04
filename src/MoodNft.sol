b53bad0828822d47bf30c90ee2be128880a1f80a
root <root@DESKTOP-KTDHBRD.localdomain>
2026-01-04 18:49:24 +0800

    feat: add MoodNft test

--- src/MoodNft.sol
+++ src/MoodNft.sol
@@ -2,23 +2,50 @@
 pragma solidity ^0.8.20;
 
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+import {Base64} from "@openzeppelin/contracts/utils/Base64.sol";
 contract MoodNft is ERC721 {
-    uint256 public tokenCounter;
-    mapping(uint256 => string) s_tokenIdToUri;
-
-    constructor() ERC721("Mood NFT", "SNFT") {
+    uint256 private tokenCounter;
+    string private s_happySvgImageUri;
+    string private s_sadSvgImageUri;
+    enum Mood {
+        HAPPY,
+        SAD
+    }
+    mapping(uint256 => Mood) private s_tokenIdToMood;
+    constructor(
+        string memory happySvgImageUri,
+        string memory sadSvgImageUri
+    ) ERC721("Mood NFT", "MN") {
         tokenCounter = 0;
+        s_happySvgImageUri = happySvgImageUri;
+        s_sadSvgImageUri = sadSvgImageUri;
     }
 
-    function mintNft(string memory tokenUri) public {
-        s_tokenIdToUri[tokenCounter] = tokenUri;
+    function mintNft() public {
+        s_tokenIdToMood[tokenCounter] = Mood.HAPPY;
+        _safeMint(msg.sender, tokenCounter);
         tokenCounter++;
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "data:application/json;base64;,";
     }
 
     function tokenURI(
         uint256 tokenId
     ) public view override returns (string memory) {
-        return s_tokenIdToUri[tokenId];
+        string memory imageURI = s_tokenIdToMood[tokenId] == Mood.HAPPY
+            ? s_happySvgImageUri
+            : s_sadSvgImageUri;
+        // abi.encodePacked return type is bytes
+        bytes memory dataURI = abi.encodePacked(
+            '{"name":"',
+            name(),
+            '","description": "An adorable PUG pup!","image": "',
+            imageURI,
+            '","attributes": [{"trait_type": "cuteness","value": 100}]}'
+        );
+        // Base64.encode return type is string
+        return string.concat(_baseURI(), Base64.encode(dataURI));
     }
 }
---
+++ test/TestMoodNft.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MoodNft} from "../src/MoodNft.sol";
+contract TestMoodNft is Test {
+    string private constant HAPPY_NFT_URI =
+        "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgdmlld0JveD0iMCAwIDEwMCAxMDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPGNpcmNsZSBjeD0iNTAiIGN5PSI1MCIgcj0iNDUiIGZpbGw9IiNGRkQ3MDAiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLXdpZHRoPSIyIi8+CiAgPGNpcmNsZSBjeD0iMzUiIGN5PSI0MCIgcj0iNSIgZmlsbD0iIzAwMCIvPgogIDxjaXJjbGUgY3g9IjY1IiBjeT0iNDAiIHI9IjUiIGZpbGw9IiMwMDAiLz4KICA8cGF0aCBkPSJNIDMwIDY1IFEgNTAgODUgNzAgNjUiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLXdpZHRoPSIzIiBzdHJva2UtbGluZWNhcD0icm91bmQiLz4KPC9zdmc+";
+    string private constant SAD_NFT_URI =
+        "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwIiBoZWlnaHQ9IjEwMCIgdmlld0JveD0iMCAwIDEwMCAxMDAiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgPGNpcmNsZSBjeD0iNTAiIGN5PSI1MCIgcj0iNDUiIGZpbGw9IiNBREQ4RTYiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLXdpZHRoPSIyIi8+CiAgPGNpcmNsZSBjeD0iMzUiIGN5PSI0MCIgcj0iNSIgZmlsbD0iIzAwMCIvPgogIDxjaXJjbGUgY3g9IjY1IiBjeT0iNDAiIHI9IjUiIGZpbGw9IiMwMDAiLz4KICA8cGF0aCBkPSJNIDM1IDc1IFEgNTAgNjAgNjUgNzUiIGZpbGw9Im5vbmUiIHN0cm9rZT0iIzAwMCIgc3Ryb2tlLXdpZHRoPSIzIiBzdHJva2UtbGluZWNhcD0icm91bmQiLz4KICA8cGF0aCBkPSJNIDM1IDQ4IEwgMzUgNTgiIHN0cm9rZT0iIzAwN0JGRiIgc3Ryb2tlLXdpZHRoPSIzIiBzdHJva2UtbGluZWNhcD0icm91bmQiLz4KICA8cGF0aCBkPSJNIDY1IDQ4IEwgNjUgNTgiIHN0cm9rZT0iIzAwN0JGRiIgc3Ryb2tlLXdpZHRoPSIzIiBzdHJva2UtbGluZWNhcD0icm91bmQiLz4KPC9zdmc+";
+
+    address USER = makeAddr("user");
+    MoodNft moodNft;
+    function setUp() public {
+        moodNft = new MoodNft(HAPPY_NFT_URI, SAD_NFT_URI);
+    }
+
+    function test_View_TokenUri() public {
+        vm.prank(USER);
+        moodNft.mintNft();
+        console.log(moodNft.tokenURI(0));
+    }
+}
